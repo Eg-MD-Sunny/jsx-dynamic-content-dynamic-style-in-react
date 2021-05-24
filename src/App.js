0d30596e92faa3e89945e3d39b8d88cc0a35b054
Naim Zaman <egnaimur@gmail.com>
2021-05-24 11:11:33 +0600

    Dynamic Style In React

--- src/App.js
+++ src/App.js
@@ -2,21 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+{/*React Dynamic Style Start*/}  
+    let style = {
+        color: 'red',
+    }
+{/*React Dynamic Style End*/}    
+
+{/*Variable Decleare [Start]*/}
+    let name = "Dr. Nila Naim";
+{/*Variable Decleare [End]*/}
+
+{/*Object Decleare [Start]*/}
+    let person = {
+        name: 'Dr. Mahfuz',
+        job: 'Singer'
+    }
+    let person2 = {
+        name: 'Eva Rahman',
+        job: 'Singer'
+    }
+{/*Object Decleare [End]*/}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+
+      {/*Dynamic Data Show Start*/}  
+        <p>Object name is: {person.name}</p>
+        <p>Object job is: {person.job}</p>
+        <p style={style}>Object name is: {person2.name + " " + person2.job}</p>
+      {/*Dynamic Data Show End*/}  
+
+    {/*Dynamic Content Start*/}
+        <h1>Doner Name is: {name}</h1>        
+        <h1>My Heading</h1>
+        <h2>Additional Value: {2+3}</h2>
+        <h2>2nd Additional Value: {(2+5)*25+65}</h2>
+        <p>My First React Paragraph</p>
+    {/*Dynamic Content End*/}  
       </header>
     </div>
   );
